fe07e3b5ab236752e9cd7a249d278f23e1056843
firdiseptiady <firdis998@gmail.com>
2018-03-29 21:41:45 +0800

    Mengisi nilai atribut

--- src/praktek2/PenjualanTasAksi.java
+++ src/praktek2/PenjualanTasAksi.java
@@ -4,6 +4,13 @@ public class PenjualanTasAksi {
     public static void main(String[] args) {
         PenjualanTas rl = new PenjualanTas();
         
+        
+        rl.harga_tas = 500000;
+        rl.jumlah_barang = 7;
+        
+        
+        
+        
    }
     
 }
