7122fd88798f9403a4c509a55d36e29589168a78
firdiseptiady <firdis998@gmail.com>
2018-03-29 21:23:41 +0800

    Membuat atribut dan behavior

--- src/praktek2/PenjualanTas.java
+++ src/praktek2/PenjualanTas.java
@@ -2,5 +2,13 @@
 package praktek2;
 
 public class PenjualanTas {
+    Integer harga_tas;
+    Integer jumlah_barang;
     
+    cetakInfo(){
+        System.out.println("===========================");
+        System.out.println("harga_tas :"+harga_tas);
+        System.out.println("jumlah_barang :"+jumlah_barang);
+        System.out.println("===========================");
+    }
 }
